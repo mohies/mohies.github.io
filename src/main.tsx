ce2816b3d4a06180298ceab2ffefb9733e5a7459
mohies <mohcen.benizza.bouzid.al@iespoligonosur.org>
2026-03-11 22:30:11 +0100

    changes tho show analytics on google

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,27 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const GA_MEASUREMENT_ID = 'G-4GGGRM78C0'
+
+const setupGoogleAnalytics = () => {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+  document.head.appendChild(script)
+
+  const inlineScript = document.createElement('script')
+  inlineScript.text = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `
+  document.head.appendChild(inlineScript)
+}
+
+setupGoogleAnalytics()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
